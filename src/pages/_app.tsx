40bfe6d0a11acd7bbfebd175f4e59fd180a072ba
Aditya <adityakumarji74@gmail.com>
2024-08-26 23:49:18 +0530

    minor change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import type { AppProps } from 'next/app'
-
-
+import type { AppProps } from "next/app";
+import "../Styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
