c4fdde69175ef8806c7858e7d13afbbb3fc29722
Aditya Singh <103613774+AdityaSingh2005@users.noreply.github.com>
2024-09-13 23:24:35 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: ['./src/styles'],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
