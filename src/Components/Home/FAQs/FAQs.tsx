738ef8164d095ddc065bbd22f4d79d95dcc7e4e9
AtharvGaur05 <laatharv@gmail.com>
2024-10-29 23:14:13 +0530

    faq-style-changed

--- src/Components/Home/FAQs/FAQs.tsx
+++ src/Components/Home/FAQs/FAQs.tsx
@@ -25,7 +25,7 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
         <button 
           className={`${styles.FAQQuestion} ${isOpen ? styles.expanded : ''}`} 
           onClick={HandleAnswer}
-          style={{ color: isOpen ? '#00ACB4' : '#fff' }}
+          style={{ color: isOpen ? '#222222' : '#222222' }}
         >
           {question}
           <Image 
@@ -33,6 +33,7 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
             alt={isOpen ? 'Collapse' : 'Expand'} 
             width={20} 
             height={20}
+            style={{backgroundColor: "gainsboro", borderRadius: "50%", fill: "red",  }}
           />
         </button>
         <div 
@@ -50,7 +51,7 @@ const FAQItem: React.FC<FAQItemProps> = ({ question, answer }) => {
 const FAQs = () => {
   const faqData = [
     {
-      question: "What is GeekRoom?",
+      question: "What is Geek Room?",
       answer: "Geek Room is a widespread coding community with over 7500+ active coders nationwide. It was started by three 1st year coding enthusiasts with the main objective to create a transparent community where sharing of ideas and helping other people is the main goal . Geek Room boasts of various hackathon winning teams , 6 independent institutes with Geek Room chapters and so much more."
     },
     {
@@ -62,7 +63,7 @@ const FAQs = () => {
       answer: "You can connect with Geek Room via our social media handles (Instagram , LinkedIn , Email , WhatsApp) etc . No , you dont need any fees to join Geek Room . You simply fill the google form , give the interview and if considered applicable , you shall be selected in a free and fair manner."
     },
     {
-      question: "What makes GeekRoom unique?",
+      question: "What makes Geek Room unique?",
       answer: "Our vast connections , a widespread community of over 7500+ coders nationwide , conduction of successful hackathons and events , boasting multi hackathon winning teams comprised purely of Geek Room members , a philosophy of a community of the coders , for the coders and by the coders and a transparent community where anyone can start coding easily , Geek Room is a unique trendsetter that is built for the upliftment of the Indian coding community."
     }
   ];
