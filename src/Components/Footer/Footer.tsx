ff32826c76e46b7f4d8bfef2a92c86f989197a4b
Naman Dadhich <namandadhich15592@gmail.com>
2024-11-06 02:49:43 +0530

    Footer updated

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from '../../styles/footer.module.scss';
-// import GeekRoomLogo from '../../../public/Images/GeekRoomLogo.svg';
-import Geek2 from "../../../public/Images/Transparent logo.png";
-import Instagram from '../../../public/Images/Instagram.svg';
-import Linkedin from '../../../public/Images/Linkedin.svg';
-import Twiiter from '../../../public/Images/Twitter.svg';
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/footer.module.scss'
+import Geek2 from "../../../public/Images/Transparent logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
+library.add(faInstagram, faLinkedin, faTwitter)
 
-const Footer = () => {
+export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.logoSection}>
-          <Image src={Geek2} alt="Footer Logo" width={120} height={120} />
-          <p className={styles.tagline}>LEARN. CONNECT. GROW.</p>
+          <div className={styles.logoWithText}>
+            <Image src={Geek2} alt="Footer Logo" width={120} height={120} />
+            <span className={styles.geekRoomText}>Geek Room</span>
+          </div>
         </div>
         <div className={styles.linksSection}>
-          <div className={styles.linkColumn}>
-            <h3>Links</h3>
-            <Link href="/">Home</Link>
-            <Link href="/about">About Us</Link>
-            <Link href="/events">Events</Link>
-            {/* <Link href="/team">Our Team</Link> */}
-            <Link href="/contact">Contact</Link>
+          <Link href="/about" className={styles.link}>About Us</Link>
+          <Link href="/events" className={styles.link}>Events</Link>
+          <Link href="#" className={styles.link}>Team</Link>
+          <Link href="#" className={styles.link}>Collaborate</Link>
+        </div>
+        <div className={styles.bottomRow}>
+          <span className={styles.copyright}>© Copyrights@GeekRoom</span>
+          <div className={styles.socialIcons}>
+            <Link href="https://www.instagram.com/_geek.room/">
+              <FontAwesomeIcon icon={faInstagram} className={styles.socialIcon} />
+            </Link>
+            <Link href="https://www.linkedin.com/company/geekr00m/mycompany/">
+              <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcon} />
+            </Link>
+            <Link href="https://x.com/geek__room_">
+              <FontAwesomeIcon icon={faTwitter} className={styles.socialIcon} />
+            </Link>
           </div>
-          <div className={styles.Socials}>
-            <h3>Socials</h3>
-            <div className={styles.SocialsLinks}>
-            <Link href="https://www.instagram.com/_geek.room/"><Image src={Instagram} alt='Instagram' style={{backgroundColor:'black'}} /></Link>
-            <Link href="https://www.linkedin.com/company/geekr00m/mycompany/"><Image src={Linkedin} alt='Linkedin' style={{backgroundColor:'black'}} /></Link>
-            <Link href="https://x.com/geek__room_"><Image src={Twiiter} alt='Twitter' style={{backgroundColor:'black'}}/></Link>
-            </div>
-          </div>
+          <span className={styles.designedBy}>Learn.Connect.Grow</span>
         </div>
       </div>
-      {/* <div className={styles.bottomBar}>
-        <p>&copy; 2024 Geek Room. All rights reserved.</p>
-      </div> */}
     </footer>
-  );
-};
-
-export default Footer;
+  )
+}
