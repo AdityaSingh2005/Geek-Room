973564d44d9d596445fb9b251d3508adf3339023
meetarora <meet.arora2403@gmail.com>
2024-11-10 16:05:45 +0530

    another9-commit

--- src/Components/OurWork/OurWork.tsx
+++ src/Components/OurWork/OurWork.tsx
@@ -1,13 +1,13 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
 import styles from '@/styles/ourWork.module.scss';
-
+import { useEffect,useRef } from 'react';
 interface WorkBoxProps {
   title: string;
   icon: string;
   icon2: string;
 }
-
 const WorkBox: React.FC<WorkBoxProps> = ({ title, icon, icon2 }) => (
   <div className={styles.box}>
     <Image src={icon} alt={title} className={styles.icon} width={200} height={200} />
@@ -17,6 +17,27 @@ const WorkBox: React.FC<WorkBoxProps> = ({ title, icon, icon2 }) => (
 );
 
 const OurWork: React.FC = () => {
+  const workPageRef = useRef<HTMLDivElement>(null);
+  const ballRef1 = useRef<HTMLDivElement>(null);
+  const ballRef2 = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (workPageRef.current && ballRef1.current && ballRef2.current) {
+        const { width, height } = workPageRef.current.getBoundingClientRect();
+        const x = e.clientX / width - 0.5;
+        const y = e.clientY / height - 0.5;
+        
+        ballRef1.current.style.transform = `translate(${-x * 500}px, ${-y * 500}px)`;
+        ballRef2.current.style.transform = `translate(${-x * 700}px, ${-y * 700}px)`;
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
   const workAreas: WorkBoxProps[] = [
     { title: 'Quality', icon: '/Images/rect4.png', icon2:'/Images/Star.png' },
     { title: 'Mentors', icon: '/Images/rect2.png', icon2:'/Images/People.png'  },
@@ -25,13 +46,15 @@ const OurWork: React.FC = () => {
   ];
 
   return (
-    <section className={styles.ourWork}>
+    <section className={styles.ourWork} ref={workPageRef}>
       <h2 className={styles.sectionTitle}>Our Work</h2>
       <div className={styles.gridContainer}>
         {workAreas.map((area, index) => (
           <WorkBox key={index} title={area.title} icon={area.icon} icon2={area.icon2} />
         ))}
       </div>
+      <div className={styles.ball1} ref={ballRef1}></div>
+      <div className={styles.ball2} ref={ballRef2}></div>
     </section>
   );
 };
